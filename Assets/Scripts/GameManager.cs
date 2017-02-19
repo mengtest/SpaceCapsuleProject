a00b0a6331f9a1b3b3979d0e4bd2d8468dccca8b
Lucas Martinez <l.martinez@student.isartdigital.com>
2017-02-19 01:09:27 +0100

    Ditched custom hitboxes, Created a game state handler class
    the gamestate handler checks if the game is in a game over state using only player coordinates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,46 +10,69 @@ public class GameManager : MonoBehaviour {
 	private Vector3 _TouchPos;
 	private Vector3 MousePos;
 
-	public float gravity;
-
-	public float Angle;
-	public float Force;
+	[SerializeField]
+	private float _gravity, _angle, _force;
 
 	public Vector Speed = new Vector(0,0);
 	public Vector Gravity;
 
 	public Capsule Player;
 
+	private bool GameOver = false; 
+	private string State;
+
 	void Start () {
-		Gravity = Vector.Acceleration(gravity/10000, -90);
-		Player = new Capsule(Force/10000, Angle);
+		
+		//Vector declarations
+		Gravity = Vector.Acceleration(_gravity/10000, -90);
+		Player = new Capsule(_force/10000, _angle);
+
+	}
+
+	//GameLoop
+	void FixedUpdate () {
+		if (!GameOver) {
+			PhysicsUpdate ();
+			DisplayUpdate ();
+			State = GameState.CheckState ();
+			GameState.MessageHandler (State, out GameOver);
+		}
+
 	}
 
 
-	void FixedUpdate () {
+	//Update sprites' positions
+	void DisplayUpdate() {
+		PlayerSprite.transform.position = new Vector2 ( Capsule.Position.x, Capsule.Position.y);
+	}
+
+	//Updates Vectors & Positions
+	void PhysicsUpdate () {
 		Speed += Gravity;
+		Capsule.Position += Speed;
 
+		switch (InputSide ()) {
+		case "left":
+			Speed += Vector.Acceleration (Player.Force, Player.Angle);
+			break;
+		case "right":
+			Speed += Vector.Acceleration (Player.Force, Player.Angle + (90 - Player.Angle) * 2);
+			break;
+		}
+	}
 
+	//Checks for touch/mouse input, returns "left" or "right"
+	string InputSide () {
 		if (Input.GetKey (KeyCode.Mouse0)) {
 
 			MousePos = Input.mousePosition; 
 			MousePos.z = 0;
 			MousePos = Camera.main.ScreenToWorldPoint (MousePos);
 
-			if ( MousePos.x > 0) {
-				Speed += Vector.Acceleration (Player.Force, Player.Angle);
-			}
-
-			if (MousePos.x <= 0) {
-				Speed += Vector.Acceleration (Player.Force, Player.Angle + (90 - Player.Angle) * 2);
-			}
+			if ( MousePos.x > 0) return "left";
+			if ( MousePos.x <= 0) return "right";
 		}
-		Player.Position += Speed;
-
-		PlayerSprite.transform.position = new Vector2 ( Player.Position.x, Player.Position.y);
+		return null;
 	}
-
-
-
 
 }
